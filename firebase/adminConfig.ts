1b3db4b99c46a1cfb70a204dcd8b2e74d9a581bd
raulsansal <raul.sanchezs@politicas.unam.mx>
2025-10-24 14:15:46 -0600

    25-10-24 Arreglo variables entorno e inicio de sesión correcta con mail, userName y Google.

--- firebase/adminConfig.ts
+++ firebase/adminConfig.ts
@@ -4,8 +4,8 @@ import { getAuth } from "firebase-admin/auth";
 
 // Obtener las variables de entorno
 const serviceAccount = {
-  type: process.env.NEXT_PUBLIC_FIREBASE_TYPE,
-  project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
